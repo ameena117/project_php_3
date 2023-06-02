689678682749ffa37245fa9d2d76f40d9c1ea55c
ameena117 <ameenajadallah@gmail.com>
2023-06-03 01:12:04 +0300

    first commit 3

--- home.php
+++ home.php
@@ -14,7 +14,7 @@ header('Location:function.php');
 </head>
 <body>
     <h1>Welcome To Home Page</h1>
-    <a href="logout.php">Log Out</a>
+    <a href="index.php">Log Out</a>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
